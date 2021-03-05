cb4353209af68874365fd0d5040f9addb8795627
Lam Nhan <lamnhan92@gmail.com>
2021-03-05 10:38:03 +0700

    update shared

--- src/lib/types/shared.type.ts
+++ src/lib/types/shared.type.ts
@@ -1,32 +1,34 @@
+import {Category} from '../services/category.service';
+import {Tag} from '../services/tag.service';
+import {Profile} from '../services/profile.service';
+import {Bundle} from '../services/bundle.service';
+
 export interface Ids {
-  '#'?: number;
+  '#'?: number; // row number or index or numerical id
   title: string;
   id: string;
-  // alias of id
-  $key?: string;
-  // alias of id / clean version of the title (if the id is un-readable)
-  slug?: string;
+  $key?: string; // alias of id
+  slug?: string; // alias of id / clean version of the title (if the id is un-readable)
 }
 
 export interface Status {
-  // blank = 'draft'
-  status?: 'draft' | 'published' | 'archived';
+  status?: 'draft' | 'publish' | 'archive' | 'trash'; // blank = 'draft'
 }
 
 export interface Timing {
-  createdAt?: string;
+  createdAt?: string; // ISO string
   updatedAt?: string;
 }
 
 export interface Authors {
   // John Doe
   // John Doe, Another John
-  // { author-1: ..., author-2: ... }
-  authors?:
-    | string
-    | {
-        [id: string]: unknown;
-      };
+  textAuthors?: string;
+  authors?: {
+    // author-1: ...
+    // author-2: ...
+    [id: string]: Profile;
+  };
 }
 
 export interface Images {
@@ -36,18 +38,20 @@ export interface Images {
 
 export interface Content {
   content?: string;
+  contentSrc?: string;
 }
 
 export interface Parents {
-  // { bundle-1: ... }
   parents?: {
-    [id: string]: unknown;
+    // bundle-1: ...
+    [id: string]: Bundle;
   };
 }
 
 export interface Relationships {
-  // { "posts/post-1": ... }
   relationships?: {
+    // "posts#post-1": ...
+    // "pages#page-2": ...
     [to: string]: unknown;
   };
 }
@@ -55,20 +59,20 @@ export interface Relationships {
 export interface Taxonomies {
   // Cat 1
   // Cat 1, Cat 2
-  // { cat-1: ..., cat-2: ... }
-  categories?:
-    | string
-    | {
-        [id: string]: unknown;
-      };
+  textCategories?: string;
+  categories?: {
+    // cat-1: ...
+    // cat-2: ...
+    [id: string]: Category;
+  };
   // Tag 1
   // Tag 1, Tag 2
-  // { tag-1: ..., tag-2: ... }
-  tags?:
-    | string
-    | {
-        [id: string]: unknown;
-      };
+  textTags?: string;
+  // tag-1: ...
+  // tag-2: ...
+  tags?: {
+    [id: string]: Tag;
+  };
 }
 
 export interface Rating {
@@ -91,5 +95,6 @@ export interface Statistics {
 }
 
 export interface Extras {
-  metas?: Record<string, unknown>;
+  metaId?: string; // standalone meta
+  metas?: Record<string, unknown>; // direct metas
 }
