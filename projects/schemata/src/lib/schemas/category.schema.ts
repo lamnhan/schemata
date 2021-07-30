fd3dc5c666efa4a8d980d161fecddd82f2a96c63
Lam Nhan <lamnhan92@gmail.com>
2021-07-30 22:27:54 +0700

    use type as only for category

--- projects/schemata/src/lib/schemas/category.schema.ts
+++ projects/schemata/src/lib/schemas/category.schema.ts
@@ -3,12 +3,12 @@ import {Ids, Status, Timing, Images} from '../shared.type';
 export interface Category extends Ids, Status, Timing, Images {
   // Ids
   uid?: string;
+  type?: string; // post-default, post-article, page-default, ...
   // Status
   // Timing
   // Images
   description?: string;
   count?: number;
-  only?: string[]; // posts, pages, posts:post, posts:typex, ...
   locale?: string;
   origin?: string;
 }
